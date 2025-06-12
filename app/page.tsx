47e39f249821079d4c6aae8aa13ed072ba60fea9
Onlook <git@onlook.com>
2025-06-12 09:18:43 -0300

    coloque animacoes de micro interacao na secao de beneficios

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Page() {
                     data-oid="mzr19b0"
                 >
                     <div
-                        className="absolute inset-0 bg-gradient-to-r from-gray-900 via-black to-black left-0 top-0 h-[1203px] -mt-16"
+                        className="absolute inset-0 bg-gradient-to-r from-gray-900 via-black to-black left-0 top-0 h-[1200px]"
                         data-oid="0_6oj_x"
                     ></div>
                     <div
@@ -108,7 +108,7 @@ export default function Page() {
 
                 {/* Right Half - Image Section */}
                 <div
-                    className="lg:w-1/2 bg-gray-900 relative overflow-visible flex items-center justify-center h-[700px] lg:h-[1200px]"
+                    className="lg:w-1/2 bg-gray-900 relative overflow-visible flex items-center justify-center pt-16 h-[700px] lg:h-[1200px]"
                     data-oid=":o1hn7l"
                 >
                     <div className="absolute inset-0 bg-black overflow-visible" data-oid="dv._hxe">
